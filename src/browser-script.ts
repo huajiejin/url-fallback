7079e642d2b3f98dd1c44a2cfd705f1bfbd382ab
Jin <jinshub@hotmail.com>
2022-04-05 23:11:09 +0800

    refactor: make browser script simpler

--- src/browser-script.ts
+++ src/browser-script.ts
@@ -1,12 +1,8 @@
 import { addErrorListener } from './public-api'
 
-let removeErrorListener = null
-
 const el = document.querySelector('script[data-url-fallback]')
 if (el instanceof HTMLScriptElement) {
-	const url = el.dataset.url
-	const fallbacks = el.dataset.fallbacks?.split(',')
-	removeErrorListener = addErrorListener({ rules: [ { url, fallbacks } ] })
+	const url = el.getAttribute('data-url')
+	const fallbacks = el.getAttribute('data-fallbacks')?.split(',')
+	addErrorListener({ rules: [ { url, fallbacks } ] })
 }
-
-export { addErrorListener, removeErrorListener }
