48106e71a8663c713b9ca5f6682470116e6a7c8b
Jin <jinshub@hotmail.com>
2022-04-05 22:47:02 +0800

    refactor: add some custom commands for index.html example

--- cypress/integration/index_spec.ts
+++ cypress/integration/index_spec.ts
@@ -1,8 +1,17 @@
-describe('url fallback', () => {
-  it('basic test', () => {
-    cy.visit('index.html')
-    cy.get('script[data-basic-test]')
-    cy.get('link[data-basic-test]')
-    cy.get('img[data-basic-test]')
+describe('index.html', () => {
+  before(() => {
+    cy.visit('/index.html')
+  })
+
+  it('should retry resources', () => {
+    cy.get('script[data-cy]').should('have.attr', 'src', 'http://127.0.0.1:9999/assets/example.js')
+    cy.get('link[data-cy]').should('have.attr', 'href', 'http://127.0.0.1:9999/assets/example.css')
+    cy.get('img[data-cy]').should('have.attr', 'src', 'http://127.0.0.1:9999/assets/example.png')
+  })
+
+  it('should load resources successfully', () => {
+    cy.get('body').should('contain.text', 'example.js loaded')
+    cy.get('body').pseudoEl('after', 'content').should('eq', 'example.css loaded')
+    cy.get('img[data-cy]').naturalWidth().should('be.greaterThan', 0)
   })
 })
---
+++ cypress/support/index.ts
@@ -0,0 +1,32 @@
+/// <reference types="cypress" />
+
+declare global {
+  namespace Cypress {
+    interface Chainable {
+			pseudoEl(pseudoEl: string, key: string): Chainable<string>
+			naturalWidth(): Chainable<any>
+    }
+  }
+}
+
+Cypress.Commands.add(
+	'pseudoEl',
+	{ prevSubject: 'element' },
+	($els: JQuery<HTMLElement>, pseudoEl: string, key: string) => {
+		const el = $els.get(0)
+		const win = el.ownerDocument.defaultView
+		const cssAfter = win.getComputedStyle(el, pseudoEl)
+		return cy.wrap(cssAfter.getPropertyValue(key).replace(/(^")|("$)/g, ''))
+	},
+)
+
+Cypress.Commands.add(
+	'naturalWidth',
+	{ prevSubject: 'element' },
+	($els: JQuery<HTMLImageElement>) => {
+		const el = $els.get(0)
+		return cy.wrap(el.naturalWidth)
+	},
+)
+
+export {}
